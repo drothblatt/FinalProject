e172803b9bcd38e724e98a867496282c0caff67f
Randy Higgins <randy@homeport.(none)>
2014-12-31 00:32:16 -0500

    Smoothed out the cycling on tester and differentiated between AI controlled and player controlled characters

--- Player.java
+++ Player.java
@@ -3,7 +3,7 @@ public class Player{
     Random r = new Random();
     private double cash;
     private int[] dice = new int[6];
-    public int[] bid = {0,0};
+    public int[] bid = {1,1};
 
     public Player(){
 	cash = 500;   
@@ -18,6 +18,50 @@ public class Player{
       cash = amount;
       }
     */
+    public String wordBid(){
+	String s1 = "";
+	String s2 = "";
+	if(bid[1] == 0)
+	    s1 = "zero";
+	if(bid[1] == 1)
+	    s1 = "one";
+	if(bid[1] == 2)
+	    s1 = "two";
+	if(bid[1] == 3)
+	    s1 = "three";
+	if(bid[1] == 4)
+	    s1 = "four";
+	if(bid[1] == 5)
+	    s1 = "five";
+	if(bid[1] == 6)
+	    s1 = "six";
+	if(bid[1] == 7)
+	    s1 = "seven";
+	if(bid[1] == 8)
+	    s1 = "eight";
+	if(bid[1] == 9)
+	    s1 = "nine";
+	if(bid[1] == 10)
+	    s1 = "ten";
+	if(bid[1] > 10)
+	    s1 = ""+bid[1];
+	if(bid[0] == 1)
+	    s2 = "ones";
+	if(bid[0] == 2)
+	    s2 = "twos";
+	if(bid[0] == 3)
+	    s2 = "threes";
+	if(bid[0] == 4)
+	    s2 = "fours";
+	if(bid[0] == 5)
+	    s2 = "fives";
+	if(bid[0] == 6)
+	    s2 = "sixes";
+	if(bid[0] == -1)
+	    return "liar";
+	return s1 +" "+ s2;
+    }
+	
 
     public void addCash(double amount){
 	cash += amount;
@@ -27,19 +71,22 @@ public class Player{
 	cash -= amount;
 	other.addCash(amount);
     }
+    
     public int[] bid(){
 	int[] b = new int[2];
-	System.out.println("make bid");
+	System.out.println("enter face value (bid '-1' for liar)");
 	Scanner s = new Scanner(System.in);
-	for(int i = 0; i < 2; i++){
-	    b[i] = s.nextInt();
+	b[0]=s.nextInt();
+	if(b[0] != -1){
+	    System.out.println("choose number of dice");
+	    b[1] = s.nextInt();
 	}
-	return b; 
+	return b;
     }
     public boolean bid(int faceVal, int dieCount){
 	int[] b = bid();
 	if(faceVal <= 6 && b[0] <= 6 && b[0] > 0){
-	    if((faceVal < b[0] && dieCount <= b[1])||(faceVal <= b[0] && dieCount < b[1])){
+	    if((faceVal >= b[0] && dieCount < b[1])||(faceVal < b[0] && dieCount <= b[1])){
 		bid = b;
 		return true;
 	    }
@@ -77,5 +124,37 @@ public class Player{
     public void reset(){
 	bid[0] = 0;
 	bid[1] = 0;
+    }
+    public void bidAI(int faceVal, int dieCount){
+	int a = howMany(faceVal) + 2;
+	for(int i = 0; i < dice.length; i++){
+	    if( r.nextInt(6) == 0)
+		a++;
+	}
+	if(dieCount > a){
+	    bid[0] = -1;
+	}
+	if(dieCount <= a){
+	    boolean other = true;
+	    for(int i = 0; i < 2; i++){
+		int c = r.nextInt(6) + 1;
+		int x = 2 * howMany(c) + 2;
+		if(x > a){
+		    other = false;
+		    i++;
+		    if(c > faceVal){
+			bid[0] = c;
+			bid[1] = (dieCount + howMany(c) / 3);
+		    }else{
+			bid[0] = c;
+			bid[1] = (dieCount + 1);
+		    }
+		}
+	    }
+	    if(other){
+		bid[0] = faceVal;
+		bid[1] = (dieCount + 1);
+	    }
+	}
     }
 }
